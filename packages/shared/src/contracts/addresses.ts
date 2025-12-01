4c4a2f69854f0e2c8a6ffd00faa8f24855f8923b
Alvi Dervishaj <alvidervishaj9@gmail.com>
2025-12-01 14:04:19 +0100

    feat: Deploying updated contracts

--- packages/shared/src/contracts/addresses.ts
+++ packages/shared/src/contracts/addresses.ts
@@ -7,7 +7,7 @@ export const contractAddresses: Record<Network, { factory: Address }> = {
     factory: '0x73511669fd4de447fed18bb79bafeac93ab7f31f' as Address, // Will be set after deployment
   },
   sepolia: {
-    factory: '0x0000000000000000000000000000000000000000' as Address, // Will be set after deployment
+    factory: '0xf9c8dc1b2312d4eeea441f0df35758e019108bce' as Address as Address, // Will be set after deployment
   },
 };
 
